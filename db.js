4913e1071489f755fa9de75ef39a76980cd14060
Yana <yana@MacBook-Pro-Yana.local>
2025-02-02 14:30:01 +0300

    env

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const db = require('knex')({
-  client: "pg", 
+  client: "pg",
   connection: {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
-    password: process.env.password,
+    password: process.env.PASSWORD,
   },
 });
 
